88e64e99ffff6ceaa1967719cccbad2186764553
Potyomko Veronika <vnpotomko@gmail.com>
2023-12-25 18:11:33 +0200

    commit

--- src/components/service/FetchMovies.jsx
+++ src/components/service/FetchMovies.jsx
@@ -7,19 +7,19 @@ async function FetchHomeApi() {
         const response = await axios.get(`${URL}trending/all/week?api_key=${KEY}&language=en-US`);
         return await response.data;
 }
-async function FetchMovieInfo({ params }) {
+async function FetchMovieInfo({params}) {
     console.log(params);
         const response = await axios.get(`${URL}/movie/${params.movieId}?api_key=${KEY}&language=en-US`);
         return response.data;
 }
 async function FetchCastApi({ params }) {
     console.log(params);
-    const response = await axios.get(`${URL}movie/${params.movieId}/credits?api_key=${KEY}&language=en-US`)
+    const response = await axios.get(`${URL}movie/${params.movieId}/credits?api_key=${KEY}`);
     return response.data;
 }
 async function FetchReviewsApi({params}) {
-        const response = await axios.get(`${URL}movie/${params.movieId}/reviews?api_key=${KEY}&language=en-US`)
-        return response.data;
+    const response = await axios.get(`${URL}movie/${params.movieId}/reviews?api_key=${KEY}`);
+    return response.data;
 }
 const api = {
     FetchHomeApi,
--- src/router.jsx
+++ src/router.jsx
@@ -1,40 +1,40 @@
-import { App } from "components/App";
-import { createBrowserRouter } from "react-router-dom";
-import { Home } from './components/pages/Home';
-import { MoviesList } from './components/pages/MoviesList';
-import api from './components/service/FetchMovies'
-import { MovieInfo } from "components/pages/MovieInfo";
-import { Cast } from "components/pages/Cast";
-import Reviews from "components/pages/Reviews";
+import { createBrowserRouter} from "react-router-dom"
+import api from "./components/service/FetchMovies"
+import { App } from "components/App"
+import { Home } from "components/pages/Home"
+import { MoviesList } from "components/pages/MoviesList"
+import { MovieInfo } from "components/pages/MovieInfo"
+import { Cast } from "components/pages/Cast"
+import Reviews from "components/pages/Reviews"
 
 export const router = createBrowserRouter([
     {
         path: '/',
-        element: <App />,
+        element: <App/>, 
         children: [
-            {
+            { 
                 index: true,
-                element: <Home/>,
+                element: <Home/>,               
             },
             {
                 path: '/movies',
-                element: <MoviesList />,
-                loader: api.FetchHomeApi,
-            },
+                element: <MoviesList/>,
+                loader:  api.FetchHomeApi,
+            }, 
             {
                 path: '/movies/:movieId',
-                element: <MovieInfo/>,
-                loader: api.FetchMovieInfo,
+                element: <MovieInfo/>, 
+                loader:  api.FetchMovieInfo,
                 children: [
                     {
                         path: '/movies/:movieId/cast',
-                        element: <Cast />,
-                        loader: api.FetchCastApi,
-                    },
+                        element: <Cast/>,
+                        loader:  api.FetchCastApi,
+                    }, 
                     {
-                        path: '/movies/:movieId/reviews',
-                        element: <Reviews />,
-                        loader: api.FetchReviewsApi
+                        path: '/movies/:movieId/revirews',
+                        element: <Reviews/>,
+                        loader: api.FetchReviewsApi,
                     }
                 ]
             }
